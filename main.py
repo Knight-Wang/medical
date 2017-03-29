a0f04153798a77eab67f80550e9bf2f9730bc30d
wangyiming <wangyiming1111@gmail.com>
2017-03-29 19:56:33 +0800

    add WLM model

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import SimRank as sr
 import DataBase as db
 from Preprocess import *
 import copy
+import math
 
 reload(sys)
 sys.setdefaultencoding('utf8')
@@ -21,7 +22,7 @@ def transform(x):
     return x
 
 
-def cal(norm1, norm2, sim_rank):
+def cal(norm1, norm2, sim_rank, net):
     """ 计算两个标准疾病名称间的相似度
     :param norm1: 标准疾病名称1
     :param norm2: 标准疾病名称2
@@ -30,41 +31,64 @@ def cal(norm1, norm2, sim_rank):
     """
     ok1 = transform(norm1)
     ok2 = transform(norm2)
+    if ok1 == ok2:
+        return 1.0
     if ok1 not in sim_rank.keys():
         return None
+    res = 0.0
+    flag = False
+    flag2 = False
     for tup in range(len(sim_rank[ok1])):
         if sim_rank[ok1][tup][0] == ok2:
-            return sim_rank[ok1][tup][1]
+            res = sim_rank[ok1][tup][1]
+            flag = True
+    for tup in range(len(net[ok1])):
+        if net[ok1][tup][0] == ok2:
+            res *= net[ok1][tup][1]
+            flag2 = True
+    if res > 0.0 and flag and flag2:
+        return res
     return None
 
 
-def cal_plus(can_name, neigh, sim_rank):
-    """ 计算某个非标准疾病名称 m 的一个候选标准疾病名称 can_name 的所有邻居
-        和 m 的其中一个标准疾病名称邻居 neigh 的平均相似度
+def cal_plus(G, can_name, neigh, sim_rank, net):
+    """ 计算某个非标准疾病名称 m 的一个候选标准疾病名称 can_name 的邻居集合
+        与 m 的伴病标准疾病名称邻居集合 neigh 的平均相似度
+    :param G: 伴病网络
     :param can_name: m 的一个候选标准疾病名称
-    :param neigh: m 的其中一个标准疾病名称邻居
+    :param neigh: m 的伴病标准疾病名称邻居集合
     :param sim_rank: simrank 相似度矩阵
     :return: 平均相似度
     """
     ok1 = transform(can_name)
-    ok2 = transform(neigh)
-    if ok1 not in sim_rank.keys():
-        return None
+    if ok1 not in G.keys():
+        return None, None
+    if not len(G[ok1]):
+        return None, None
+
     sum = 0.0
     cnt = 0
-    for tup in range(len(sim_rank[ok1])):
-        can_neigh = sim_rank[ok1][tup][0]
-        tmp = cal(can_neigh, ok2, sim_rank)
-        if tmp:
-            sum += tmp
-            cnt += 1
+    neigh_sim = {}
+    for x in G[ok1]:
+        neigh_avg = 0.0
+        cnt_avg = 0
+        for n in neigh:
+            tmp = cal(x, n, sim_rank, net)
+            if tmp:
+                neigh_avg += tmp
+                cnt_avg += 1
+                sum += tmp
+                cnt += 1
+        if cnt_avg:
+            neigh_avg /= cnt_avg
+            neigh_sim[x] = neigh_avg
     if cnt:
-        return sum / cnt
-    return None
+        return sum / cnt, neigh_sim
+    return None, None
 
 
 def dic2list(dic):
-    """ 将字典dic按照value（相似度）排序（从大到小）后放入列表中返回
+    """ 将字典dic按照value（相似度）排序（降序）后放入列表中返回
     :param dic: 字典 <key, value> = <标准疾病名称, 相似度>
     :return: [(标准疾病名称1, 相似度1), [(标准疾病名称2, 相似度2), ...](降序)
     """
@@ -73,18 +97,19 @@ def dic2list(dic):
     return l
 
 
-def classify(bad_one, candidate, good_neigh, sim_mat):
+def classify(G, bad_one, candidate, good_neigh, sim_mat, net):
     """ 将非标准疾病名称 bad_one 分类
     :param bad_one: 非标准疾病名称
     :param candidate: 候选名称字典 <key, value> = <标准疾病名称, 相似度>
     :param good_neigh: 所有非标准疾病名称的伴病字典 <key, value> = <非标准疾病名称, set(标准伴病1, 标准伴病2, ...)>
     :param sim_mat: simrank 相似度矩阵
+    :param net: 边权调整
     :return: simrank 计算后的候选名称字典 <key, value> = <标准疾病名称, 相似度>,
              是否经过 simrank 计算(相似度很高不用计算, bad_one 没出现过或没有邻居无法计算),
              具体到每个邻居的相似度字典 <key, value> = <邻居, 相似度>
     """
     res = {}
-    neigh_sim = {}
+    neigh_sim_dic = {}
     can_list = dic2list(candidate)
     if abs(can_list[0][1] - 1.0) <= 1e-5 or \
        (bad_one not in good_neigh.keys()) or \
@@ -92,32 +117,25 @@ def classify(bad_one, candidate, good_neigh, sim_mat):
         return candidate, False, None
     if can_list[0][1] > 0.857:  # 减小噪声，如果排名第一的候选相似度很高（大于0.857），就不再进行sim_rank
         return candidate, False, None
-    flag = False
+    flag = True
     for c, sim in can_list:
-        neigh_sim[c] = []
-        sum_s = 0.0
-        cnt_s = 0
-        for gn in good_neigh[bad_one]:
-            tmp = cal_plus(c, gn, sim_mat)
-            if tmp:
-                neigh_sim[c].append((gn, tmp))
-                sum_s += tmp
-                cnt_s += 1
-        if cnt_s:
-            sum_s /= cnt_s
-        if sum_s > 0.0:
-            flag = True
-        res[c] = sum_s
+        tmp, neigh_sim = cal_plus(G, c, good_neigh[bad_one], sim_mat, net)
+        if not tmp:
+            flag = False
+            break
+        else:
+            res[c] = tmp
+            neigh_sim_dic[c] = neigh_sim
     if flag:
-        return res, True, neigh_sim
-    return candidate, False, neigh_sim
+        return res, True, neigh_sim_dic
+    return candidate, False, None
 
 
 def weighting(before, after, ratio):
     """ 预处理结果和simrank处理结果加权
     :param before: simrank之前结果字典 <key, value> = <标准疾病名称, 相似度>
     :param after: simrank之后结果字典 <key, value> = <标准疾病名称, 相似度>
-    :param ratio: simrak之后所占加权系数 float in [0, 1]
+    :param ratio: simrank之后所占加权系数 float in [0, 1]
     :return: 加权结果字典 <key, value> = <标准疾病名称, 相似度>
     """
     max_val = max(val for val in after.itervalues())
@@ -191,7 +209,7 @@ def print_right_log(right_file, bad_name, before, after, label, cnt):
     right_file.writelines("=================================================\n")
 
 
-def print_wrong_log(wrong_file, bad_name, before, after, label, cnt, neigh_sim):
+def print_wrong_log(wrong_file, bad_name, before, after, label, cnt, graph, neigh_sim_dic):
     """ 把之前分对而simrank分错的记录打印到错误日志中
     :param wrong_file:
     :param bad_name:
@@ -199,7 +217,8 @@ def print_wrong_log(wrong_file, bad_name, before, after, label, cnt, neigh_sim):
     :param after:
     :param label:
     :param cnt:
-    :param neigh_sim:
+    :param graph:
+    :param neigh_sim_dic:
     :return:
     """
     wrong_file.writelines(str(cnt) + '\n')
@@ -213,13 +232,10 @@ def print_wrong_log(wrong_file, bad_name, before, after, label, cnt, neigh_sim):
     for x in range(len(after)):
         wrong_file.writelines("候选" + str(x + 1) + " --> ")
         wrong_file.writelines(after[x][0] + " : " + str(after[x][1]) + '\n')
-        wrong_file.writelines("***************************************************\n")
-        neigh = neigh_sim[after[x][0]]
-        l = len(neigh)
-        for i in range(l):
-            wrong_file.writelines("邻居" + str(i + 1) + " --> ")
-            wrong_file.writelines(neigh[i][0] + " : " + str(neigh[i][1]) + '\n')
-        wrong_file.writelines("***************************************************\n")
+        wrong_file.writelines("++++++++++++++++++++++++++++++++++\n")
+        for k, v in neigh_sim_dic[after[x][0]].iteritems():
+            wrong_file.writelines(k + str(v) + "\n")
+        wrong_file.writelines("++++++++++++++++++++++++++++++++++++\n")
     wrong_file.writelines("---------------------------------------------------\n")
     wrong_file.writelines("正确答案:\n")
     wrong_file.writelines(label + '\n')
@@ -277,7 +293,7 @@ def get_network(records, disease, surgeries):
     :return: 伴病网络字典 <key, value> = <标准疾病名称, set(标准疾病名称1, 标准疾病名称2, ...)>
              非标准疾病名称的伴病(邻居)字典 <key, value> = <非标准疾病名称, set(标准名称1, 标准名称2, ...)>
     """
-    G = {}
+    G = {}  # 伴病网络 邻接表
     bad_names = {}  # 存储非标准疾病名称和它的标准疾病名称邻居们
     appear = {}  # 单个标准疾病名称出现次数
     co_appear = {}  # <标准疾病名称1, 标准疾病名称2> 出现次数
@@ -285,7 +301,7 @@ def get_network(records, disease, surgeries):
     cnt_row = 0
     for t in records:
         cnt_row += 1
-        if cnt_row % 100 == 0:
+        if cnt_row % 100000 == 0:
             print "第 %d 行" % cnt_row
         link = set()  # 这条记录中的标准名称集合
         bad = set()  # 这条记录中的非标准名称集合
@@ -300,7 +316,7 @@ def get_network(records, disease, surgeries):
                 # if name_dict:
                     # res = dic2list(name_dict)
                     # if res[0][1] > 0.857:  # 可信度比较高，直接认为是标准疾病名称
-                if s in disease:
+                if s in disease:#
                     # 在这里解决别名问题
                     n = copy.copy(s)
                     if s in alias_dict:
@@ -348,10 +364,21 @@ def get_network(records, disease, surgeries):
             f.writelines(tmp + ' ' + str(len(G[x])) + '\n')
             for y in G[x]:
                 f.writelines(y)
+                # val = math.log(len(G) * 1.0 / len(G[x]))
                 a = min(x, y)
                 b = max(x, y)
                 val = co_appear[(a, b)] * 1.0 / appear[x]
                 f.writelines(' ' + str(val) + '\n')
+    finally:
+        f.close()
+
+    f = open("texts/out/bad_names.txt", "w")
+    try:
+        for b in bad_names:
+            f.writelines(b + "    ")
+            for n in bad_names[b]:
+                f.writelines(n + " ")
+            f.writelines("\n")
     finally:
         f.close()
     return G, bad_names
@@ -423,6 +450,110 @@ def init():
 
     return normal_disease, icd4_dic, icd3_dic, normal_surgeries, medical_records
 
+
+def cal_relatedness(g, x, y):
+    """ 计算两个实体的语义相关性
+    :param g: 网络字典 <key, value> = <实体名称, [(实体名称1, 相似度1), (实体名称2, 相似度2), ...]>
+    :param x: 实体1 string
+    :param y: 实体2 string
+    :return: 语义相关性 float in [0, 1]
+    """
+    x_neigh = {}
+    y_neigh = {}
+    union = set()
+    for i in range(len(g[x])):
+        x_neigh[g[x][i][0]] = g[x][i][1]
+        union.add(g[x][i][0])
+    for i in range(len(g[y])):
+        y_neigh[g[y][i][0]] = g[y][i][1]
+        union.add(g[y][i][0])
+
+    vec1 = []
+    vec2 = []
+    for ele in union:
+        if ele in x_neigh.keys():
+            vec1.append(x_neigh[ele])
+        else:
+            vec1.append(0.0)
+        if ele in y_neigh.keys():
+            vec2.append(y_neigh[ele])
+        else:
+            vec2.append(0.0)
+
+    n = len(vec1)
+    m1 = 0.0
+    m2 = 0.0
+    product = 0.0
+    for i in range(n):
+        product += vec1[i] * vec2[i]
+        m1 += vec1[i] * vec1[i]
+        m2 += vec2[i] * vec2[i]
+
+    return product * 1.0 / (math.sqrt(m1) * math.sqrt(m2))
+
+
+def WLM(graph_file):
+    """ 使用WLM模型计算两个实体的语义相关性
+    :param graph_file: 图文件
+    :return: 重新计算之后的网络字典 <key, value> = <实体名称, (实体名称1, 相关度1), (实体名称2, 相关度2) ...>
+    """
+    g = {}
+    f = codecs.open(graph_file, "r", "utf-8")
+    fout = open("texts/out/relatedness.txt", "w")
+    nodes = int(f.readline())
+    fout.writelines(str(nodes) + "\n")
+    while nodes:
+        nodes -= 1
+        line = f.readline()
+        arr = line.split()
+        node = arr[0]
+        g[node] = []
+        edges = int(arr[1].decode('utf-8'))
+        while edges:
+            edges -= 1
+            out = f.readline().split()
+            out_neighbor = out[0]
+            weight = float(out[1].decode('utf-8'))
+            g[node].append((out_neighbor, weight))
+    f.close()
+
+    res = {}  # 返回值
+    for x in g:
+        fout.writelines(x + " " + str(len(g[x])) + "\n")
+        res[x] = []
+        for y in g[x]:
+            val = cal_relatedness(g, x, y[0])
+            fout.writelines(y[0] + " " + str(val) + "\n")
+            res[x].append((y[0], val))
+
+    fout.close()
+    return res
+
+
+def classify_with_WLM(neigh, name_dict, network):
+    """ 使用WLM模型分类
+    :param bad_name: 非标准疾病名称
+    :param neigh: bad_name 的伴病邻居集合
+    :param name_dict: 候选实体和相关性字典 <key, value> = <候选实体名称, 相关性值>
+    :param network: WLM网络（字典）
+    :return: 使用WLM重新计算之后的候选实体和相关性字典 <key, value> = <候选实体名称, 得分>
+    """
+    for x in name_dict.keys():
+        if x not in network.keys():
+            return None
+
+    res = {}
+    for (c, sim) in name_dict.iteritems():
+        sum = 0.0
+        for n in neigh:
+            for i in range(len(network[c])):
+                if network[c][i][0] == n:
+                    sum += network[c][i][1]
+        sum /= len(network[c])
+        res[c] = sum
+    return res
+
+
 if __name__ == "__main__":
 
     normal_disease, icd4_dic, icd3_dic, normal_surgeries, medical_records = init()
@@ -437,6 +568,58 @@ if __name__ == "__main__":
         cnt_edge += len(G[x])
     print "节点数：%d" % cnt_node
     print "边数：%d" % cnt_edge
+
+    net = WLM("texts/out/graph.txt")
+    '''
+    d = db.DataBase()
+    values = d.query('select 非标准名称, 标准疾病名 from labeleddata')
+    start_time = datetime.datetime.now()
+    TopK = 1
+    cnt_similar = 0  # 相似度很高，无需再计算的个数
+    cnt_right = 0  # 正确的个数
+    cnt_wrong = 0  # 错误的个数
+    cnt_not_process = 0  # 未处理的个数
+    wrong_log = open("texts/out/WLM/wrong.txt", "w")
+    for row in values:
+        unnormalized_name = row[0].strip()
+        normalized_name = row[1].strip()
+        name_dict = get_can_dict(unnormalized_name, normal_disease, icd4_dic)
+
+        if len(name_dict) != 0:
+            if normalized_name in name_dict.keys():  # map correctly
+                l = dic2list(name_dict)
+                if l[0][1] > 0.857:
+                    cnt_similar += 1
+                    continue
+                if unnormalized_name not in bad_names:
+                    cnt_not_process += 1
+                    continue
+                tmp = classify_with_WLM(bad_names[unnormalized_name], name_dict, net)
+                if not tmp:
+                    cnt_not_process += 1
+                    continue
+                l = dic2list(tmp)
+                if verdict(l, normalized_name, TopK):
+                    cnt_right += 1
+                else:
+                    cnt_wrong += 1
+                    wrong_log.writelines("非标准疾病名称 : " + unnormalized_name + "\n")
+                    for x in l:
+                        wrong_log.writelines(x[0] + " : " + str(x[1]) + "\n")
+                    wrong_log.writelines("答案 : " + normalized_name + "\n")
+                    wrong_log.writelines("==============================================\n")
+            else:  # map to a disease name but the name is not the labeled one.
+                pass  # 待处理
+        else:  # cannot map
+            pass  # 待处理
+    end_time = datetime.datetime.now()
+    print '相似度很高无需处理的个数为 %d' % cnt_similar
+    print '未能处理的个数为 %d' % cnt_not_process
+    print '分类正确的个数为 %d' % cnt_right
+    print '分类错误的个数为 %d' % cnt_wrong
+    print '分类运行时间为 %d' % (end_time - start_time).seconds
+    wrong_log.close()
+    '''
 
     start_time = datetime.datetime.now()
     s = sr.SimRank(graph_file="texts/out/graph.txt")
@@ -471,9 +654,9 @@ if __name__ == "__main__":
                 ok = filter_map_well(unnormalized_name, name_dict, map_right_f)
                 if ok:
                     continue
-                re_rank, checked, neigh_sim = classify(unnormalized_name, name_dict, bad_names, res)
+                re_rank, checked, neigh_sim_dic = classify(G, unnormalized_name, name_dict, bad_names, res, net)
                 if checked:
-                    weighted = weighting(name_dict, re_rank, 0.5)
+                    weighted = weighting(name_dict, re_rank, 0.2)
                     weighted = dic2list(weighted)
                 re_rank = dic2list(re_rank)
                 f.writelines(str(unnormalized_name) + ':\n')
@@ -513,7 +696,7 @@ if __name__ == "__main__":
                         f.writelines('no\n')
                         if flag:
                             cnt_noise += 1
-                            print_wrong_log(wrong, unnormalized_name, rank, re_rank, normalized_name, cnt_noise, neigh_sim)
+                            print_wrong_log(wrong, unnormalized_name, rank, re_rank, normalized_name, cnt_noise, G, neigh_sim_dic)
                     f.writelines('++++++++++++++++++++++++++++++++++++++++++++\n')
                     f.writelines('加权之后结果：\n')
                     f.writelines(weighted[0][0] + '\n')
