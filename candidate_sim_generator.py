fe22291cfab69ccc69508865f05d54228dd8dde4
luoyi <luoyihh@gmail.com>
2017-04-10 20:42:52 +0800

    Encapsulate a candidate_generator

---
+++ candidate_sim_generator.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# coding=utf-8
+
+from Preprocess import *
+from util import getNormalNames, loadDict
+import copy
+import MySQLdb
+
+class candidate_sim_generator:
+
+    def __init__(self):
+        conn = MySQLdb.connect("localhost", "root", "10081008", "medical", charset='utf8')
+        cursor = conn.cursor()
+        cursor.execute('select ICD,疾病名称 from I2025')
+        values = cursor.fetchall()
+        self.normal = getNormalNames(values)  # (normalized_name, ICD-10)
+
+        self.alias_dict = loadDict("./Dict/Alias.txt")
+
+    # 返回候选实体集合
+    # is_single_segment = 1 说明诊断只有一个片段，
+    def getCandidates(self, names_segs):
+
+        name_dict_seg = getMappingResult_segs(names_segs, self.normal)
+        is_single_segment = 1 if len(names_segs) == 1 else 0
+
+        # add alias
+        segments_len = len(name_dict_seg)
+        dict_copy = copy.deepcopy(name_dict_seg)
+        for i in range(segments_len):
+            for (k, v) in dict_copy[i].iteritems():
+                if k in self.alias_dict.keys():
+                    name_dict_seg[i][self.alias_dict[k]] = v
+
+        return is_single_segment, name_dict_seg
