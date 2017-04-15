1159f55ba6752131e217580f585e39f68638bcfb
wangyiming <wangyiming1111@gmail.com>
2017-04-15 14:04:24 +0800

    xxx

--- contain_u_names_rec.py
+++ contain_u_names_rec.py
@@ -30,13 +30,13 @@ def init():
 
     print '标准手术名称个数为 %d' % len(normal_surgeries)
 
-    medical_records_2013 = d.query('select  S050100, S050200, S050600, S050700, \
-                                            S050800, S050900, S051000, S051100, \
-                                            S056000, S056100, S056200, \
-                                            S050501, S051201, S051301, S051401, \
-                                            S051501, S057001, S057101, S057201, \
-                                            S057301, S057401 \
-                                       from heart_new_2013')
+    medical_records_2013 = d.query('select  id, S050100, S050200, S050600, S050700, \
+                                                S050800, S050900, S051000, S051100, \
+                                                S056000, S056100, S056200, \
+                                                S050501, S051201, S051301, S051401, \
+                                                S051501, S057001, S057101, S057201, \
+                                                S057301, S057401 \
+                                           from heart_new_2013')
 
     # medical_records_2014_15 = d.query('select  S050100, S050200, S050600, S050700, \
     #                                            S050800, S050900, S051000, S051100, \
@@ -69,47 +69,39 @@ def get_graph(normal_diseases, normal_surgeries, medical_records, labeled_data):
              not_single -> 至少有一个邻居的 疾病 或 手术 名称（非孤立点）集合
     """
 
-    cnt_u_in = 0  # 在非标准疾病名称字典中的非标准疾病名称数量
-    cnt_u_not_in = 0  # 不在非标准疾病名称字典中的非标准疾病名称数量
-    cnt_n = 0  # 标准疾病名称的数量
     cnt_all = 0  # 医疗记录的数量
-    cnt_n_s = 0  # 标准手术名称
-    res = {}
+    f = open("filtered_records/filtered_records.txt", "w")
     for t in medical_records:
         cnt_all += 1
         if not (cnt_all % 100000):
             print cnt_all
         now = 0
+        flag = False
         for s in t:
-            s = s.strip()
             now += 1
+            if now == 1:
+                continue
+            s = s.strip()
             if not s:
                 continue
             if s == 'NA':
                 continue
-            if now <= 11:  # 这是个疾病名称
-                if s in normal_diseases.keys():
-                    cnt_n += 1
-                elif s in labeled_data.keys():
-                    cnt_u_in += 1
-                    if s not in res:
-                        res[s] = 1
-                    else:
-                        res[s] += 1
+            if now <= 12:  # 这是个疾病名称
+                if s in labeled_data.keys():
+                    flag = True
+                    break
+        if flag:
+            now = 0
+            for x in t:
+                now += 1
+                if now == 1:
+                    f.writelines("\"" + str(x) + "\"" + ",")
+                elif now != 22:
+                    f.writelines("\"" + x + "\"" + ",")
                 else:
-                    cnt_u_not_in += 1
-            else:
-                if s in normal_surgeries.keys():
-                    cnt_n_s += 1
+                    f.writelines("\"" + x + "\"")
+            f.writelines("\n")
 
-    print '记录总数 %d' % cnt_all
-    print '合法名称总数 %d' % (cnt_n + cnt_u_in + cnt_u_not_in)
-    print '标准疾病名称数量 %d' % cnt_n
-    print '在字典中的非标准疾病名称 %d' % cnt_u_in
-    print '不在字典中的非标准疾病名称 %d' % cnt_u_not_in
-    print '标准手术名称 %d' % cnt_n_s
-    for k, v in res.iteritems():
-        print k, v
 
 normal_dis, normal_sur, records, labeled_data = init()
 get_graph(normal_dis, normal_sur, records, labeled_data)
