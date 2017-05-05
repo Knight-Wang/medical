a7a20fcbc16ff03a596e29022f7a1c1c586243b2
wangyiming <wangyiming1111@gmail.com>
2017-05-05 13:56:09 +0800

    add my code

--- simrank_new/src/test.py
+++ simrank_new/src/test.py
@@ -114,6 +114,7 @@ def test_init(name_dict, no_cand):
                 continue
             cnt_all += 1
             if d[0] in no_cand:
+
                 not_in += 1
             entity_set = set()
             for e in name_dict[d[0]]:
